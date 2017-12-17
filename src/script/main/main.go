c35959d806138d2770a1a247bc2f20875c061b73
ahaha0807 <b5331@oic.jp>
2017-12-17 20:12:15 +0900

    コマンドで使えるようにした！

--- src/script/main/main.go
+++ src/script/main/main.go
@@ -4,17 +4,37 @@ import (
 	"github.com/urfave/cli"
 	"os"
 	"github.com/ahaha0807/translation/src/script/translation"
+	"fmt"
 )
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "translation-ahaha0807"
+	app.Name = "trans"
 	app.Usage = "translation on command"
-	app.Version = "0.0.1"
+	app.Version = "1.0.0"
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "english, e",
+			Usage: "英語 to 日本語",
+		},
+	}
 
 	app.Action = func(context *cli.Context) error {
-		translation.ToEnglish("")
-		return nil
+		if len(context.Args()) == 0 {
+			fmt.Println("USAGE: trans --[option] [words]")
+			return nil
+		}
+
+		if !context.Bool("english") {
+			result := translation.ToEnglish(context.Args().Get(0))
+			fmt.Println(result)
+			return nil
+		} else {
+			result := translation.ToJapanese(context.Args().Get(0))
+			fmt.Println(result)
+			return nil
+		}
 	}
 
 	app.Run(os.Args)

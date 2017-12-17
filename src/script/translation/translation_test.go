a62cb316b60425886e58fbc585829a69641ce49e
ahaha0807 <b5331@oic.jp>
2017-12-17 19:09:49 +0900

    テストの定義

---
+++ src/script/translation/translation_test.go
@@ -0,0 +1,50 @@
+package translation
+
+import (
+	"testing"
+)
+
+func TestToEnglish(t *testing.T) {
+	actual := ToEnglish("りんご")
+	expected := "Apple"
+
+	AssertTranslation(actual, expected, t)
+
+	actual = ToEnglish("耳")
+	expected = "Ear"
+
+	AssertTranslation(actual, expected, t)
+
+	actual = ToEnglish("変数")
+	expected = "Variable"
+
+	AssertTranslation(actual, expected, t)
+}
+
+func TestToJapanese(t *testing.T) {
+	actual := ToJapanese("Apple")
+	expected := "りんご"
+
+	AssertTranslation(actual, expected, t)
+
+	actual = ToJapanese("ear")
+	expected = "耳"
+
+	AssertTranslation(actual, expected, t)
+
+	actual = ToJapanese("VARIABLE")
+	expected = "変数"
+
+	AssertTranslation(actual, expected, t)
+
+}
+
+func AssertTranslation(actual string, expected string, t *testing.T) {
+	if actual == "" {
+		t.Errorf("翻訳を実行できませんでした。")
+	}
+
+	if actual != expected {
+		t.Errorf("翻訳結果が異なります %v, %v", actual, expected)
+	}
+}

db3c0752781ae4a542cc251b0b2dc1e9342194c7
ahaha0807 <b5331@oic.jp>
2017-12-17 19:10:39 +0900

    翻訳実行メソッドの定義

--- src/script/main/main.go
+++ src/script/main/main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"github.com/urfave/cli"
-	"fmt"
 	"os"
+	"github.com/ahaha0807/translation/src/script/translation"
 )
 
-func main () {
+func main() {
 	app := cli.NewApp()
 	app.Name = "translation-ahaha0807"
 	app.Usage = "translation on command"
 	app.Version = "0.0.1"
 
-	app.Action = func (context *cli.Context) error {
-		fmt.Println("Hello world on cli")
+	app.Action = func(context *cli.Context) error {
+		translation.ToEnglish("")
 		return nil
 	}
 
---
+++ src/script/translation/translation.go
@@ -0,0 +1,81 @@
+package translation
+
+import (
+	"os"
+	"net/http"
+	"io/ioutil"
+	"net/url"
+	"regexp"
+	"fmt"
+)
+
+func ToEnglish(str string) string {
+	apiToken := getAPIToken()
+
+	rawResult := doTranslate(str, "ja", "en", apiToken)
+	fmt.Println(rawResult)
+	result := format(rawResult)
+
+	return result
+}
+
+func ToJapanese(str string) string {
+
+	return ""
+}
+
+func getAPIToken() string {
+	apiKey := string(os.Getenv("MICROSOFT_TRANSLATE_APIKEY"))
+
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("POST", "https://api.cognitive.microsoft.com/sts/v1.0/issueToken", nil)
+	req.Header.Set("Ocp-Apim-Subscription-Key", string(apiKey))
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	apiToken, _ := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+
+	return string(apiToken)
+}
+
+func doTranslate(str string, srcLanguage string, destLanguage string, token string) string {
+	host := "https://api.microsofttranslator.com"
+	path := "/V2/Http.svc/Translate"
+
+	values := url.Values{}
+	values.Set("to", destLanguage)
+	values.Add("from", srcLanguage)
+	values.Add("text", str)
+
+	option := values.Encode()
+
+	client := &http.Client{}
+
+	s := host + path + "?" + option
+
+	req, _ := http.NewRequest("GET", s, nil)
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	res, err := client.Do(req)
+	if err != nil {
+		return ""
+	}
+	defer res.Body.Close()
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	return string(body)
+}
+
+func format(raw string) string {
+	re := regexp.MustCompile(">.*<")
+	result := string(re.Find([]byte(raw)))
+
+	return result[1:len(result)-1]
+}
